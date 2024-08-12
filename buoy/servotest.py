508285473c20bafd7d9e59536968d8dad5d47a5b
wfairmanOlin <wfairman@olin.edu>
2024-08-12 12:09:03 -0400

    added servotest code

---
+++ buoy/servotest.py
@@ -0,0 +1,29 @@
+import json, logging, time, smtplib, os
+from time import sleep
+from smbus2 import SMBus, i2c_msg
+from gpiozero.pins.pigpio import PiGPIOFactory
+from gpiozero import Servo
+
+folder = "Desktop/HAUCS/"
+#Load Buoy ID from param file
+with open(folder + "buoy/param.json") as file:
+        param = json.load(file)
+
+##### SERVO #####
+#Initialize Servo
+servo = Servo(18, max_pulse_width=0.0023, pin_factory=PiGPIOFactory())
+
+def wobble(secs):
+    mv_time = 0.4
+    cycles = int(secs//(2 * mv_time))
+    for i in range(cycles):
+        servo.value = param['high']
+        sleep(mv_time)
+        servo.value = param['low']
+        sleep(mv_time)
+    servo.value = param['neutral']
+    sleep(mv_time)
+    servo.detach()
+
+
+wobble(60)
