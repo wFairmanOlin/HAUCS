24be05a089a99996ebc384223f8bf111ccdf8e64
wfairmanOlin <wfairman@olin.edu>
2024-09-17 18:53:44 -0400

    changed startup script

--- buoy/startup.sh
+++ buoy/startup.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+pulled_something="Already up to date."
+
 cd /home/haucs/Desktop/HAUCS
 
 sleep 60
@@ -6,8 +8,8 @@ status_resp=$(git status -s --untracked-files=no)
 
 if [ -z "$status_resp" ]
 then
-    pull_resp=$(git pull origin main -q)
-    if [ -z "$pull_resp" ]
+    pull_resp=$(git pull origin main)
+    if [[ "$pull_resp" == *"$pulled_something"* ]]
     then
         echo "nothing pulled"
     else
