482ecd7be0bf3d6f7cbab7e83a153e048cf701b8
wfairmanOlin <wfairman@olin.edu>
2024-09-17 22:31:53 -0400

    updated startup again ...

--- buoy/startup.sh
+++ buoy/startup.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
-pulled_nothing="Already up to date."
+pulled_something="Unpacking objects"
 
 cd /home/haucs/Desktop/HAUCS
 
 sleep 30
 status_resp=$(git status -s --untracked-files=no)
 
+# if no files have been changed
 if [[ -z "$status_resp" ]]
 then
     pull_resp=$(git pull origin main)
-    if [[ "$pull_resp" != *"$pulled_nothing"* ]]
+    #if local repo is updated to remote
+    if [[ "$pull_resp" == *"$pulled_something"* ]]
     then
-        echo "pulled $pull_resp"
-        echo "rebooting"
-        sleep 60
-        sudo reboot
+        echo "succesfully updated"
+        echo "$pull_resp"
     else
         echo "pulled nothing, already up to date"
     fi
