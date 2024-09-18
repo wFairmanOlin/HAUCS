f69f0208efbcca0b83034592d402deef8a9f1e94
wfairmanOlin <wfairman@olin.edu>
2024-09-17 22:10:12 -0400

    updated startup bash script

--- buoy/startup.sh
+++ buoy/startup.sh
@@ -1,24 +1,25 @@
 #!/bin/bash
-pulled_something="Already up to date."
+pulled_nothing="Already up to date."
 
 cd /home/haucs/Desktop/HAUCS
 
-sleep 60
+sleep 30
 status_resp=$(git status -s --untracked-files=no)
 
-if [ -z "$status_resp" ]
+if [[ -z "$status_resp" ]]
 then
     pull_resp=$(git pull origin main)
-    if [[ "$pull_resp" == *"$pulled_something"* ]]
+    if [[ "$pull_resp" != *"$pulled_nothing"* ]]
     then
-        echo "nothing pulled"
-    else
+        echo "pulled $pull_resp"
         echo "rebooting"
-        sleep 5
+        sleep 60
         sudo reboot
+    else
+        echo "pulled nothing, already up to date"
     fi
 else
-    echo "local changes, won't pull"
+    echo "local changes present, can't pull"
 fi
-echo "running  main script"
+echo "running main script"
 /home/haucs/buoy/bin/python3 buoy/main.py
